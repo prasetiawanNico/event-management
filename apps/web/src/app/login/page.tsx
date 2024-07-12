40ab0896bdf6fb2474a6db2acc1099dbec5468e3
nicoPrasetiawan <127836772+nicoPrasetiawan@users.noreply.github.com>
2024-07-12 15:54:20 +0700

    feat: add layout for header, footer, login and register, add necessary packages (#3)

---
+++ apps/web/src/app/login/page.tsx
@@ -0,0 +1,163 @@
+'use client';
+
+import { useState } from 'react';
+import { Formik, Form, Field, FormikProps } from 'formik';
+import * as Yup from 'yup';
+
+import { ILogin } from '../interfaces/login.interface';
+import {
+  Box,
+  Container,
+  TextField,
+  Typography,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+} from '@mui/material';
+import { useRouter } from 'next/navigation';
+
+const loginSchema = Yup.object({
+  username: Yup.string().required('Username is required'),
+  password: Yup.string()
+    .required('Password is required')
+    .min(5, 'Password must be at least 6 characters'),
+});
+
+const initialValues: ILogin = {
+  username: '',
+  password: '',
+};
+
+function Login() {
+  const router = useRouter();
+  const [open, setOpen] = useState(false);
+
+  const handleSubmit = (values: any, { setSubmitting, setErrors }: any) => {
+    if (values.username === 'admin' && values.password === 'admin') {
+      router.push('/');
+    } else {
+      setOpen(true);
+      setSubmitting(false);
+      setErrors({ password: 'Invalid username or password' });
+    }
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Container
+      maxWidth="sm"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        minHeight: '100vh',
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          backgroundColor: '#f9f9f9',
+          padding: 4,
+          borderRadius: 5,
+          boxShadow: 3,
+        }}
+      >
+        <Typography component="h1" variant="h4" sx={{ mb: 2 }}>
+          Login
+        </Typography>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={loginSchema}
+          onSubmit={handleSubmit}
+        >
+          {(props: FormikProps<ILogin>) => {
+            const { values, errors, touched, handleChange, isSubmitting } =
+              props;
+
+            return (
+              <Form>
+                <Box sx={{ mt: 3 }}>
+                  <Field
+                    as={TextField}
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    id="username"
+                    label="Username"
+                    type="username"
+                    name="username"
+                    onChange={handleChange}
+                    value={values.username}
+                    error={touched.username && Boolean(errors.username)}
+                    helperText={touched.username && errors.username}
+                  />
+                  <Field
+                    as={TextField}
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    id="password"
+                    name="password"
+                    label="Password"
+                    type="password"
+                    onChange={handleChange}
+                    value={values.password}
+                    error={touched.password && Boolean(errors.password)}
+                    helperText={touched.password && errors.password}
+                  />
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    disabled={isSubmitting}
+                    sx={{ mt: 3, mb: 2 }}
+                  >
+                    LOGIN
+                  </Button>
+                </Box>
+              </Form>
+            );
+          }}
+        </Formik>
+      </Box>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        PaperProps={{
+          sx: {
+            padding: '2rem',
+            borderRadius: '0.5rem',
+            boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+            animation: 'fadeIn 0.3s ease-out',
+          },
+        }}
+      >
+        <DialogTitle id="alert-dialog-title">Invalid Login</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Invalid username or password. Please try again.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </Container>
+  );
+}
+
+export default Login;
--- apps/web/src/components/Header.tsx
+++ apps/web/src/components/Header.tsx
@@ -1,3 +1,48 @@
-export const Header = () => {
-  return <div>Header</div>;
+'use client';
+
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+
+const Header: React.FC = () => {
+  const path: string = usePathname();
+
+  return (
+    <AppBar position="static" color="transparent" elevation={6}>
+      <Toolbar sx={{ justifyContent: 'space-between', padding: '0 2rem' }}>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Link href="/" passHref>
+              <img
+                src="/appLogo.svg"
+                alt="Logo"
+                style={{ height: '30px', marginRight: '10px' }}
+              />
+            </Link>
+            EVENTICA
+          </Typography>
+        </Box>
+        <Box sx={{ display: 'flex', gap: 2 }}>
+          <Button color="inherit">Our events</Button>
+          <Button color="inherit">Community</Button>
+          <Button color="inherit">Contact</Button>
+          <Button
+            href="/login"
+            variant={path.startsWith('/login') ? 'contained' : 'outlined'}
+          >
+            Login
+          </Button>
+          <Button
+            href="/register"
+            variant={path.startsWith('/register') ? 'contained' : 'outlined'}
+          >
+            Register
+          </Button>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 };
+
+export default Header;
